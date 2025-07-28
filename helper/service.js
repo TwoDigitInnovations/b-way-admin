fef65b48837f92088a70a765a86b6e913d7b1fa8
2Digit Innovations <106655815+TwoDigitInnovations@users.noreply.github.com>
2025-07-28 21:52:54 +0530

    Update service.js

--- helper/service.js
+++ helper/service.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 // const ConstantsUrl = "http://localhost:8000";
-// const ConstantsUrl = process.env.NODE_ENV === 'development' ? "http://localhost:8000" : 'https://api.b-way.net';
+// const ConstantsUrl = process.env.NODE_ENV === 'development'? "http://localhost:8000" : 'https://api.b-way.net';
 
 const ConstantsUrl = "https://api.b-way.net";
 
